88abb0f491a495596dd30c0062c7e456c64718cd
Maverick Shreeve <maverickshreeve@gmail.com>
2022-05-10 19:14:33 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return(
+    <div>
+      
+      <Navbar/>
+    </div>
+
+  ) 
+};
+
+//yarn start in cd hat website to start server
+
+export default App;
